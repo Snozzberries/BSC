dbe48ac07a69a31db267b3b7afe574c89b571b16
Soule <misoule@sentinel.com>
2018-09-05 11:58:49 -0700

    New-SelectionPrompt

---
+++ Get-AwsRunningConfig.ps1
@@ -0,0 +1,76 @@
+﻿if(-not(Get-Module -Name "AWSPowerShell")){if(-not(Import-Module AWSPowerShell)){"Install AWS PowerShell";exit;}}
+
+$profiles = Get-AWSCredential -ListProfileDetail
+if($profiles.ProfileName.Count -gt 1)
+{
+    $i = 0
+    $profiles|%{
+        "$($i): $($_.ProfileName)"
+        $i++
+    }
+    $temp = Read-Host "Press the number for the corresponding profile"
+    $i = [Convert]::ToInt32($temp,10)
+    if($i -is [int])
+    {
+        $profile = $profiles.ProfileName[$i]
+    }
+    else
+    {
+        "Input was not numeric"
+    }
+}
+else
+{
+    $profile = $profiles.ProfileName
+}
+
+$regions = Get-AWSRegion
+if($regions.IsShellDefault)
+{
+    $region = $regions|?{$_.IsShellDefault}
+    $temp = Read-Host "Use default region ($region)? (y/n)"
+    if($temp -notin @("y","n"))
+    {
+        "Input not y or n"
+    }
+    elseif($temp -eq "y")
+    {
+        $region = $regions|?{$_.IsShellDefault}
+    }
+    else
+    {
+        $i = 0
+        $regions|%{
+            "$($i): $($_.Region) - $($_.Name)"
+            $i++
+        }
+        $temp = Read-Host "Press the number for the corresponding region"
+        $i = [Convert]::ToInt32($temp,10)
+        if($i -is [int])
+        {
+            $region = $regions.Region[$i]
+        }
+        else
+        {
+            "Input was not numeric"
+        }
+    }
+}
+else
+{
+    $i = 0
+    $regions|%{
+        "$($i): $($_.Region) - $($_.Name)"
+        $i++
+    }
+    $temp = Read-Host "Press the number for the corresponding region"
+    $i = [Convert]::ToInt32($temp,10)
+    if($i -is [int])
+    {
+        $region = $regions.Region[$i]
+    }
+    else
+    {
+        "Input was not numeric"
+    }
+}
---
+++ New-DomainController.ps1
@@ -0,0 +1,26 @@
+﻿if(-not(Get-Module -Name "AWSPowerShell")){if(-not(Import-Module AWSPowerShell)){"Install AWS PowerShell";exit;}}
+
+$profiles = Get-AWSCredential -ListProfileDetail
+if($profiles.ProfileName.Count -gt 1)
+{
+    $i = 0
+    $profiles|%{
+        "$($i): $($_.ProfileName)"
+        $i++
+    }
+    $input = Read-Host "Press the number for the corresponding profile"
+    $i = [Convert]::ToInt32($input,10)
+    if($i -is [int])
+    {
+        $profile = $profiles.ProfileName[$i]
+    }
+    else
+    {
+        "Input was not numeric"
+    }
+}
+else
+{
+    $profile = $profiles.ProfileName
+}
+
