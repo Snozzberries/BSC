69ae13555bfd6adfdb0aaecd8a4bebf558751f8c
Soule <misoule@sentinel.com>
2018-09-05 12:19:35 -0700

    Name change and new file

---
+++ Get-Dependencies.ps1
@@ -0,0 +1,17 @@
+﻿$dependencies = @(
+    "https://raw.githubusercontent.com/Snozzberries/BSC/master/New-SelectionPrompt.psm1"
+)
+
+foreach($dependency in $dependencies)
+{
+    $content = Invoke-WebRequest $dependency
+    $path = "$env:TEMP\$(Get-Random -Maximum 999 -Minimum 100)-importModule.psm1"
+    Out-File -FilePath $path -InputObject $content
+    
+    if(-not (Import-Module $path))
+    {
+        Write-Error "Failed to import module:`n $dependency"
+    }
+
+    Remove-Item $path
+}
